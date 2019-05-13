d378a2aa4be2fef79e1bd09f338f09ea9a6d68a9
Joseph Weidinger <josephweidinger@carfax.com>
2019-05-13 18:48:40 -0500

    remove accidentally committed dist files

--- packages/time/dist/formatted-date.d.ts
+++
@@ -1,8 +0,0 @@
-import { GraphQLObjectType } from 'graphql';
-export declare const FormattedDate: GraphQLObjectType<Date, any, any>;
-export declare const resolvers: {
-    [fieldName: string]: import("graphql").GraphQLFieldResolver<any, any, {
-        [argName: string]: any;
-    }>;
-};
-export declare const typeDefs: string;
