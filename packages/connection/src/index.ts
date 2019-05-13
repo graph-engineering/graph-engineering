9fa80d3334c1ed23e141657ab137f177cd01cda3
Joseph Weidinger <josephweidinger@carfax.com>
2019-05-12 20:14:51 -0500

    minor refactor to extract connection lib out of test graphql server

--- packages/connection/src/index.ts
+++ packages/connection/src/index.ts
@@ -1,3 +1,21 @@
-import { makeConnection } from './first-gen-connection-maker'
+import gql from 'graphql-tag'
 
-export { makeConnection }
+export { makeConnection } from './first-gen-connection-maker'
+
+export const typeDefs = gql`
+	type PageInfo {
+		hasPreviousPage: Boolean!
+		hasNextPage: Boolean!
+		startCursor: String
+		endCursor: String
+	}
+
+	interface Connection {
+		pageInfo: PageInfo!
+		totalCount: Int!
+	}
+
+	interface ConnectionEdge {
+		cursor: String!
+	}
+`
