a07de26d62a98ed301fc98e9934b270e33a5059e
Joseph Weidinger <josephweidinger@carfax.com>
2019-06-09 12:11:32 -0500

    remove .dist because it was accidentally committed (also updated gitignore rules in an earlier commit)

--- packages/prelude/.dist/Option.d.ts
+++
@@ -1,8 +0,0 @@
-import * as Option from "fp-ts/lib/Option";
-export * from "fp-ts/lib/Option";
-export declare const chain: import("fp-ts-contrib/lib/Do").Do1<"Option", {}>;
-export declare const merge: <A>(
-  first: Option.Option<A>,
-  second: Option.Option<A>,
-  merge: (first: A, second: A) => A
-) => Option.Option<A>;
