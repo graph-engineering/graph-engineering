62ffd1eaf40c9e6b2f197eb5bdd7dae2848b357b
Conner Ruhl <connerruhl@me.com>
2019-07-12 13:16:42 -0500

    Using `from` as standard constructor name instead of `of` and making `Error` better

--- packages/prelude/src/Either.ts
+++ packages/prelude/src/Either.ts
@@ -1,14 +1,14 @@
 export * from "fp-ts/lib/Either";
 import * as Either from "fp-ts/lib/Either";
 
-import { chainOf } from ".";
+import { chainFrom } from ".";
 import * as Error from "./Error";
 import { Fn } from "./FP";
 
 export type ErrorOr<A, L extends Error = Error> = Either.Either<L, A>;
 
-export const chained = chainOf(Either.either);
+export const chained = chainFrom(Either.either);
 
 export const tryCatch = Either.tryCatch2v;
 export const tryCatchError = <A>(fn: Fn.Lazy<A>): ErrorOr<A> =>
-  tryCatch(fn, Error.of);
+  tryCatch(fn, Error.from);
--- packages/prelude/src/TaskEither.ts
+++ packages/prelude/src/TaskEither.ts
@@ -1,20 +1,20 @@
 export * from "fp-ts/lib/TaskEither";
 import * as TaskEither from "fp-ts/lib/TaskEither";
 
-import { chainOf } from ".";
+import { chainFrom } from ".";
 import * as Either from "./Either";
 import * as Error from "./Error";
 import { Fn } from "./FP";
 
 export type ErrorOr<A, L extends Error = Error> = TaskEither.TaskEither<L, A>;
 
-export const chained = chainOf(TaskEither.taskEither);
+export const chained = chainFrom(TaskEither.taskEither);
 
 export const fromRight = <A, L = Error>(a: A): TaskEither.TaskEither<L, A> =>
   TaskEither.fromEither(Either.right(a));
 
 export const tryCatchError = <A>(fn: Fn.Lazy<Promise<A>>): ErrorOr<A> =>
-  TaskEither.tryCatch(fn, Error.of);
+  TaskEither.tryCatch(fn, Error.from);
 
 export const runUnsafe = <A>(
   taskEither: TaskEither.TaskEither<any, A>
