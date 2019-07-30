4ec7a564e5ee18e12d4ecbb7c75d331db80cba00
Joseph Weidinger <josephweidinger@carfax.com>
2019-07-30 10:27:07 -0500

    remove graphql-extra for now

--- packages/graphql-extra/.dist/Document/Prefix.d.ts
+++
@@ -1,3 +0,0 @@
-import * as GraphQL from "..";
-export declare const name: (prefix: string, name: string) => string;
-export declare const document: (prefix: string, document: GraphQL.DocumentNode) => GraphQL.DocumentNode;
--- packages/graphql-extra/.dist/Schema/Resolvers.d.ts
+++
@@ -1,5 +0,0 @@
-export interface Resolvers {
-    readonly [typeName: string]: unknown;
-}
-export declare const prefix: (prefix: string, resolvers: Resolvers) => Resolvers;
-export declare const merge: (first: Resolvers, second: Resolvers) => Resolvers;
