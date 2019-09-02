f4a3b9c9f275b304ec063e7db7c5e1b1e849d75a
Joseph Weidinger <jsphweid@gmail.com>
2019-09-02 16:49:30 -0500

    halfway

--- packages/adapters/src/utils/flexible-calculator.ts
+++ packages/adapters/src/utils/flexible-calculator.ts
@@ -1,97 +1,70 @@
-import { Either, Error, pipe } from "@grapheng/prelude";
+import { Either, Error, pipe } from '@grapheng/prelude'
 
-import {
-  RatioTableWithNumbersOrRelationshipFunctions,
-  RatioTableWithOnlyNumbers,
-  RatioTableWithOnlyRelationshipFunctions
-} from "./types";
+import { makeTableAsFunctions } from './helpers'
+import { RatioTableWithNumbersOrRelationshipFunctions, RatioTableWithOnlyNumbers } from './types'
 
 // TODO: make more typesafe
 // TODO: curry when available
-export const compareObjects = <T extends object, K extends object>(
-  include: boolean,
-  input: T,
-  lookup: K
-): T =>
-  Object.entries(input)
-    .filter(([field]) => !!lookup[field as keyof K] === include)
-    .reduce(
-      (previous, [key, value]) => ({ ...previous, [key]: value }),
-      // tslint:disable-next-line:no-object-literal-type-assertion
-      {} as T
-    );
+export const compareObjects = <T extends object, K extends object>(include: boolean, input: T, lookup: K): T =>
+	Object.entries(input)
+		.filter(([field]) => !!lookup[field as keyof K] === include)
+		.reduce(
+			(previous, [key, value]) => ({ ...previous, [key]: value }),
+			// tslint:disable-next-line:no-object-literal-type-assertion
+			{} as T
+		)
 
 const objectValuesAreAllNumbers = <T>(obj: T): Either.ErrorOr<T> =>
-  Either.fromPredicate<Error, T>(
-    input => Object.values(input).every(value => typeof value === "number"),
-    Error.fromL("Not all the fields in the object are numbers.")
-  )(obj);
+	Either.fromPredicate<Error, T>(
+		input => Object.values(input).every(value => typeof value === 'number'),
+		Error.fromL('Not all the fields in the object are numbers.')
+	)(obj)
 
 const objectIsNotEmpty = <T>(obj: T): Either.ErrorOr<T> =>
-  Either.fromPredicate<Error, T>(
-    input => Object.keys(input).length > 0,
-    Error.fromL(
-      "Flexible Calculator must have at least one key that exists in the provided ratio table."
-    )
-  )(obj);
-
-const makeTableAsFunctions = (
-  table: RatioTableWithNumbersOrRelationshipFunctions
-): RatioTableWithOnlyRelationshipFunctions =>
-  Object.entries(table).reduce(
-    (previous, [key, value]) => ({
-      ...previous,
-      [key]:
-        typeof value === "number"
-          ? {
-              toBaseUnit: (num: number) => num * value,
-              fromBaseUnit: (num: number) => num / value
-            }
-          : value
-    }),
-    // tslint:disable-next-line:no-object-literal-type-assertion
-    {} as RatioTableWithOnlyRelationshipFunctions
-  );
+	Either.fromPredicate<Error, T>(
+		input => Object.keys(input).length > 0,
+		Error.fromL('Flexible Calculator must have at least one key that exists in the provided ratio table.')
+	)(obj)
 
 // TODO: output is GenericRatioTable
 export const flexibleCalculator = (
-  input: Partial<RatioTableWithOnlyNumbers>,
-  _table: RatioTableWithNumbersOrRelationshipFunctions
+	input: Partial<RatioTableWithOnlyNumbers>,
+	_table: RatioTableWithNumbersOrRelationshipFunctions
 ): Either.ErrorOr<RatioTableWithOnlyNumbers> =>
-  pipe(
-    makeTableAsFunctions(_table),
-    table =>
-      pipe(
-        Either.right(compareObjects(true, input, table)),
-        Either.chain(objectIsNotEmpty),
-        Either.chain(objectValuesAreAllNumbers),
-        Either.map(refinedInput => compareObjects(false, table, refinedInput)),
-        Either.map(Object.keys),
-        Either.map(uncalculatedInputFields =>
-          pipe(
-            Object.entries(input)[0],
-            ([firstItemUnit, firstItemUnitValue]) =>
-              uncalculatedInputFields.reduce(
-                (previous, inputField) => ({
-                  ...previous,
-                  [inputField]: pipe(
-                    firstItemUnitValue as number,
-                    table[firstItemUnit].toBaseUnit,
-                    table[inputField].fromBaseUnit
-                  )
-                }),
-                // tslint:disable-next-line:no-object-literal-type-assertion
-                {} as Partial<RatioTableWithOnlyNumbers>
-              )
-          )
-        ),
-        Either.map(
-          recentlyCalculatedFieldsMap =>
-            // tslint:disable-next-line:no-object-literal-type-assertion
-            ({
-              ...recentlyCalculatedFieldsMap,
-              ...compareObjects(true, input, table)
-            } as RatioTableWithOnlyNumbers)
-        )
-      )
-  );
+	pipe(
+		makeTableAsFunctions(_table),
+		table =>
+			pipe(
+				Either.right(compareObjects(true, input, table)),
+				Either.chain(objectIsNotEmpty),
+				Either.chain(objectValuesAreAllNumbers),
+				Either.map(refinedInput => compareObjects(false, table, refinedInput)),
+				Either.map(Object.keys),
+				Either.map(uncalculatedInputFields =>
+					pipe(
+						Object.entries(input)[0],
+						([firstItemUnit, firstItemUnitValue]) =>
+							uncalculatedInputFields.reduce(
+								(previous, inputField) => ({
+									...previous,
+									[inputField]: pipe(
+										firstItemUnitValue as number,
+										table[firstItemUnit].toBaseUnit,
+										table[inputField].fromBaseUnit
+									)
+								}),
+								// tslint:disable-next-line:no-object-literal-type-assertion
+								{} as Partial<RatioTableWithOnlyNumbers>
+							)
+					)
+				),
+				Either.map(
+					recentlyCalculatedFieldsMap =>
+						// tslint:disable-next-line:no-object-literal-type-assertion
+						({
+							...recentlyCalculatedFieldsMap,
+							...compareObjects(true, input, table)
+						} as RatioTableWithOnlyNumbers)
+				)
+			)
+	)
--- packages/adapters/src/utils/helpers.ts
+++ packages/adapters/src/utils/helpers.ts
@@ -1,62 +1,64 @@
-import { pipe } from "@grapheng/prelude";
-import {
-  graphql,
-  GraphQLFieldResolver,
-  GraphQLObjectType,
-  GraphQLSchema,
-  printType
-} from "graphql";
-import gql from "graphql-tag";
+import { pipe } from '@grapheng/prelude'
+import { graphql, GraphQLFieldResolver, GraphQLObjectType, GraphQLSchema, printType } from 'graphql'
+import gql from 'graphql-tag'
+import { RatioTableWithNumbersOrRelationshipFunctions, RatioTableWithOnlyRelationshipFunctions } from './types'
 
-export const extractResolvers = (
-  object: GraphQLObjectType
-): { readonly [fieldName: string]: GraphQLFieldResolver<any, any> } =>
-  Object.entries(object.getFields()).reduce(
-    (previous, [name, field]) => ({ ...previous, [name]: field.resolve }),
-    {}
-  );
+export const extractResolvers = (object: GraphQLObjectType): { readonly [fieldName: string]: GraphQLFieldResolver<any, any> } =>
+	Object.entries(object.getFields()).reduce((previous, [name, field]) => ({ ...previous, [name]: field.resolve }), {})
 
-export function expectSimpleObjectType(
-  graphQLObjectType: GraphQLObjectType,
-  value: any,
-  queryString: string
-): jest.Matchers<Promise<any>> {
-  const schema = new GraphQLSchema({
-    query: new GraphQLObjectType({
-      name: "Query",
-      fields: {
-        arbitraryRootField: {
-          type: graphQLObjectType,
-          resolve: () => value
-        }
-      }
-    })
-  });
+export function expectSimpleObjectType(graphQLObjectType: GraphQLObjectType, value: any, queryString: string): jest.Matchers<Promise<any>> {
+	const schema = new GraphQLSchema({
+		query: new GraphQLObjectType({
+			name: 'Query',
+			fields: {
+				arbitraryRootField: {
+					type: graphQLObjectType,
+					resolve: () => value
+				}
+			}
+		})
+	})
 
-  return expect(
-    graphql(schema, `{ arbitraryRootField ${queryString} }`).then(
-      response => {
-        // tslint:disable-next-line:no-if-statement
-        if (response.errors) throw new Error(response.errors[0].message);
-        else return response.data && response.data.arbitraryRootField;
-      }
-      // response.errors
-      //   ? fail(new Error(response.errors[0].message))
-      //   : response.data && response.data.arbitraryRootField
-    )
-  );
+	return expect(
+		graphql(schema, `{ arbitraryRootField ${queryString} }`).then(
+			response => {
+				// tslint:disable-next-line:no-if-statement
+				if (response.errors) throw new Error(response.errors[0].message)
+				else return response.data && response.data.arbitraryRootField
+			}
+			// response.errors
+			//   ? fail(new Error(response.errors[0].message))
+			//   : response.data && response.data.arbitraryRootField
+		)
+	)
 }
 
 export const createGraphQLExports = (graphQLType: GraphQLObjectType) => ({
-  typeDefs: gql`
-    ${printType(graphQLType)}
-  `,
-  resolvers: extractResolvers(graphQLType),
-  rawType: graphQLType
-});
+	typeDefs: gql`
+		${printType(graphQLType)}
+	`,
+	resolvers: extractResolvers(graphQLType),
+	rawType: graphQLType
+})
 
 export const getObjectKeysAsSelection = (type: object): string =>
-  pipe(
-    Object.keys(type).join(" "),
-    fields => `{ ${fields} }`
-  );
+	pipe(
+		Object.keys(type).join(' '),
+		fields => `{ ${fields} }`
+	)
+
+export const makeTableAsFunctions = (table: RatioTableWithNumbersOrRelationshipFunctions): RatioTableWithOnlyRelationshipFunctions =>
+	Object.entries(table).reduce(
+		(previous, [key, value]) => ({
+			...previous,
+			[key]:
+				typeof value === 'number'
+					? {
+							toBaseUnit: (num: number) => num * value,
+							fromBaseUnit: (num: number) => num / value
+					  }
+					: value
+		}),
+		// tslint:disable-next-line:no-object-literal-type-assertion
+		{} as RatioTableWithOnlyRelationshipFunctions
+	)
