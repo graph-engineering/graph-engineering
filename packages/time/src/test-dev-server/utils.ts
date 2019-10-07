ed178a119127c937b2fa609930311c918d184c32
Joseph Weidinger <josephweidinger@carfax.com>
2019-10-07 12:18:40 -0500

    delete time library

--- packages/time/src/test-dev-server/utils.ts
+++
@@ -1,9 +0,0 @@
-import { ApolloServer } from "apollo-server";
-import { GraphQLSchema } from "graphql";
-
-export const runApolloServer = (schema: GraphQLSchema): void => {
-  new ApolloServer({ schema }).listen().then(({ url }) => {
-    // tslint:disable-next-line:no-console
-    console.log(`🚀 Server ready at ${url}`);
-  });
-};
